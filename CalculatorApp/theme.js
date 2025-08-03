100b31638031819a1599287a28e454f0bfa46bc4
Charbel Estephan <charbelstephan12@gmail.com>
2025-08-03 16:07:14 +0300

    final touches

--- CalculatorApp/theme.js
+++ CalculatorApp/theme.js
@@ -8,17 +8,17 @@ export const themes = {
   },
 
   teal: {
-    background: '#002b36', // Deep blue-gray
-    text: '#EBEFD0', // White text
-    B1: '#32DBC6', // Dark muted teal
-    B2: '#008080', // Classic teal (subtle, balanced)
-    B3: '#49BEB7', // Vibrant cyan-teal for strong contrast
+    background: '#002b36',
+    text: '#EBEFD0',
+    B1: '#32DBC6',
+    B2: '#008080',
+    B3: '#49BEB7',
   },
-  Red: {
-    background: '#44000D', // Black
-    text: '#F9D276', // White
-    B1: '#83142C', // Charcoal gray
-    B2: '#AD1D45', // Deep neon purple
-    B3: '#ff3c77ff', // Bright neon green
+  Bordeaux: {
+    background: '#44000D',
+    text: '#F9D276',
+    B1: '#83142C',
+    B2: '#AD1D45',
+    B3: '#ff3c77ff',
   },
 };
